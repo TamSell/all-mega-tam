09f6470e47d181e4db0fa455247775c61e4f9925
TamSell <tellorsam@gmail.com>
2024-07-26 15:59:34 -0400

    Reformatted BinToDev, starting simple calculator

--- BinToDec/BinToDec.cpp
+++ BinToDec/BinToDec.cpp
@@ -2,39 +2,58 @@
 #include <string>
 
 
+std::string takeInput(std::string message);
+int myExpon(int base, int exp);
 
-std::string takeInput(std::string message)
+void calcPrintBintoDec();
+void calcPrintDectoBin();
+
+int main()
 {
-	std::string input;
-
-	std::cout << message << ": " << '\n' << '\n';
-
-	while (!std::getline(std::cin, input))
-	{
-		std::cout << "\nNot a valid input please try again: \n";
+	std::string userIn;
+	while (1) {
+		userIn = takeInput("Enter dec to translate from binary to decimal, enter bin to translate from decimal to binary");
+		if (userIn == "exit")
+		{
+			break;
+		}
+		else if (userIn == "bin") {
+			calcPrintBintoDec();
+		}
+		else if (userIn == "dec")
+		{
+			calcPrintDectoBin();
+		}
 	}
-
-	std::cout << '\n';
-
-	return input;
 }
 
-
-int myExpon(int base, int exp)
+void calcPrintDectoBin()
 {
-	int output = 0;
-	int holder = 1;
-	if (exp == 0)
-	{
-		return 1;
-	}
-	for (int i = 0; i < exp; ++i)
-	{
-		holder *= base;
-	}
-	return output + holder;
-}
+	int input = stoi(takeInput("Enter a decimal number to be changed into binary system"));
+	int inOut = input;
+	int iter = 0;
+	int binOut[16] = {};
 
+	for (int i = 0; input > 0; i++)
+	{
+		binOut[i] = input % 2;
+		input /= 2;
+	}
+
+	std::cout << "Dec In: " << inOut << '\n';
+	std::cout << "Bin out: ";
+
+	for (int i = (sizeof(binOut) / sizeof(binOut[0])) - 1; i >= 0; i--)
+	{
+		if ((i + 1) % 4 == 0)
+		{
+			std::cout << ' ';
+		}
+		std::cout << binOut[i];
+	}
+
+	std::cout << '\n' << '\n';
+}
 
 void calcPrintBintoDec()
 {
@@ -61,53 +80,34 @@ void calcPrintBintoDec()
 	std::cout << "Dec out: " << decOut << '\n' << '\n';
 }
 
-void calcPrintDectoBin()
+
+int myExpon(int base, int exp)
 {
-	int input = stoi(takeInput("Enter a decimal number to be changed into binary system"));
-	int inOut = input;
-	int iter = 0;
-	int binOut[16] = {};
-
-	for (int i = 0; input > 0; i++)
+	int output = 0;
+	int holder = 1;
+	if (exp == 0)
 	{
-		binOut[i] = input % 2;
-		input /= 2;
+		return 1;
 	}
-
-	std::cout << "Dec In: " << inOut << '\n';
-	std::cout << "Bin out: ";
-
-	for (int i = (sizeof(binOut) / sizeof(binOut[0])) - 1; i >=0 ; i--)
+	for (int i = 0; i < exp; ++i)
 	{
-		if ((i+1) % 4 == 0)
-		{
-			std::cout << ' ';
-		}
-		std::cout << binOut[i];
+		holder *= base;
 	}
-
-	std::cout << '\n' << '\n';
-
-
-
+	return output + holder;
 }
 
-
-int main()
+std::string takeInput(std::string message)
 {
-	std::string userIn;
-	while (1) {
-		userIn = takeInput("Enter dec to translate from binary to decimal, enter bin to translate from decimal to binary");
-		if (userIn == "exit")
-		{
-			break;
-		}
-		else if (userIn == "bin") {
-			calcPrintBintoDec();
-		}
-		else if (userIn == "dec")
-		{
-			calcPrintDectoBin();
-		}
+	std::string input;
+
+	std::cout << message << ": " << '\n' << '\n';
+
+	while (!std::getline(std::cin, input))
+	{
+		std::cout << "\nNot a valid input please try again: \n";
 	}
+
+	std::cout << '\n';
+
+	return input;
 }
