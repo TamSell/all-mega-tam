6787234b37b23f47f7fdae245da2fcbdb5049cc9
TamSell <tellorsam@gmail.com>
2024-07-26 15:40:50 -0400

    Finished binary to decimal and back

--- BinToDec/BinToDec.cpp
+++ BinToDec/BinToDec.cpp
@@ -1,4 +1,23 @@
 #include <iostream>
+#include <string>
+
+
+
+std::string takeInput(std::string message)
+{
+	std::string input;
+
+	std::cout << message << ": " << '\n' << '\n';
+
+	while (!std::getline(std::cin, input))
+	{
+		std::cout << "\nNot a valid input please try again: \n";
+	}
+
+	std::cout << '\n';
+
+	return input;
+}
 
 
 int myExpon(int base, int exp)
@@ -17,26 +36,78 @@ int myExpon(int base, int exp)
 }
 
 
-int main()
+void calcPrintBintoDec()
 {
-	int ten = 1010;
-	int one = 0001;
-	int test = ten & one;
+	int input = stoi(takeInput("Enter a binary number to be changed into decimal system"));
+	int inOut = input;
+	int one = 1;
 	int iter = 0;
 	int decOut = 0;
-	while (ten > 0)
+	while (input > 0)
 	{
-		if (ten & one)
+		if (input & one)
 		{
-			ten = ten / 10;
+			input = input / 10;
 			decOut += myExpon(2, iter);
 		}
 		else {
-			ten = ten / 10;
+			input = input / 10;
 		}
 		++iter;
 
 	}
 
-		std::cout << "Dec out: " << decOut;
+	std::cout << "Bin In: " << inOut << '\n';
+	std::cout << "Dec out: " << decOut << '\n' << '\n';
+}
+
+void calcPrintDectoBin()
+{
+	int input = stoi(takeInput("Enter a decimal number to be changed into binary system"));
+	int inOut = input;
+	int iter = 0;
+	int binOut[16] = {};
+
+	for (int i = 0; input > 0; i++)
+	{
+		binOut[i] = input % 2;
+		input /= 2;
+	}
+
+	std::cout << "Dec In: " << inOut << '\n';
+	std::cout << "Bin out: ";
+
+	for (int i = (sizeof(binOut) / sizeof(binOut[0])) - 1; i >=0 ; i--)
+	{
+		if ((i+1) % 4 == 0)
+		{
+			std::cout << ' ';
+		}
+		std::cout << binOut[i];
+	}
+
+	std::cout << '\n' << '\n';
+
+
+
+}
+
+
+int main()
+{
+	std::string userIn;
+	while (1) {
+		userIn = takeInput("Enter dec to translate from binary to decimal, enter bin to translate from decimal to binary");
+		if (userIn == "exit")
+		{
+			break;
+		}
+		else if (userIn == "bin") {
+			calcPrintBintoDec();
+		}
+		else if (userIn == "dec")
+		{
+			calcPrintDectoBin();
+		}
+	}
 }
