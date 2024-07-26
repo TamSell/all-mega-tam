47eed90fcfcbd6aee8048c68eef24615339c0b2a
TamSell <tellorsam@gmail.com>
2024-07-26 08:27:58 -0400

    Got binary to dec working had to think before bed

--- BinToDec/BinToDec.cpp
+++ BinToDec/BinToDec.cpp
@@ -1,31 +1,42 @@
 #include <iostream>
 
+
+int myExpon(int base, int exp)
+{
+	int output = 0;
+	int holder = 1;
+	if (exp == 0)
+	{
+		return 1;
+	}
+	for (int i = 0; i < exp; ++i)
+	{
+		holder *= base;
+	}
+	return output + holder;
+}
+
+
 int main()
 {
-	std::cout << 101010 % 1 << '\n';
-
 	int ten = 1010;
 	int one = 0001;
 	int test = ten & one;
-	int iter = 1;
+	int iter = 0;
 	int decOut = 0;
-	while (ten >= 0)
+	while (ten > 0)
 	{
-		if (ten | one == 0){
-			ten -= (10 * iter);
-		}
-		
-		if (iter == 1){
-			decOut += 1;
-			++iter;
-			ten -= (10 * iter);
+		if (ten & one)
+		{
+			ten = ten / 10;
+			decOut += myExpon(2, iter);
 		}
 		else {
-			decOut += (2 * iter);
-			++iter;
-			ten -= (10 * iter);
+			ten = ten / 10;
 		}
+		++iter;
+
 	}
 
-	std::cout << test;
+		std::cout << "Dec out: " << decOut;
 }
